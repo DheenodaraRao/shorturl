ca8d8d33f9b5e27a7be74794fe4cb678e00cad63
Dheenodara <dheenodararao@gmail.com>
2020-03-05 22:28:36 +0800

    Completed Initial UI Design

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {url: '', shortUrl: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({url: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.url);
+    event.preventDefault();
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>
+          Short URL
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <label>Url: 
+          <input className="Url-input" type="url" value={this.state.url} onChange={this.handleChange}/>
+        </label>
+        
+        <input className="submitBtn" value={"Shorthen"} type="submit" onSubmit={this.handleSubmit}/>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={this.state.shortUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {this.state.shortUrl}
         </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
